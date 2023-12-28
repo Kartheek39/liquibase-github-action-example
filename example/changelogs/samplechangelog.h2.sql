4cb550e2948938e21fead50aca067ec005b9a8c3
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2023-12-28 16:25:53 +0530

    Update samplechangelog.h2.sql

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,18 +1,6 @@
---liquibase formatted sql
-
---changeset kartheek:1
---rollback DROP TABLE person;
-create table person (
-    id int primary key,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
-
 --changeset kartheek:2
 --rollback DROP TABLE company;
-create table company (
+create table company2 (
     id int primary key,
     name varchar(50) not null,
     address1 varchar(50),
@@ -20,15 +8,3 @@ create table company (
     city varchar(30),
     last_update date
 )
-
---changeset other.dev:3
---rollback ALTER TABLE person DROP COLUMN country;
-alter table person add column country varchar(2)
-
---changeset other.dev:4
---rollback ALTER TABLE person DROP COLUMN state;
-alter table person add column state varchar(2)
-
---changeset other.dev:5
---rollback ALTER TABLE company DROP COLUMN country;
-alter table company add column country varchar(2)

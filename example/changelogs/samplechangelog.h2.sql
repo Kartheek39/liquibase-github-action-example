05a2a81153244ecc7c8139d820608df073cc233c
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2023-12-28 16:49:30 +0530

    Update samplechangelog.h2.sql

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,12 +1,9 @@
--- --changeset kartheek:2
--- --rollback DROP TABLE company;
--- create table company2 (
---     id int primary key,
---     name varchar(50) not null,
---     address1 varchar(50),
---     address2 varchar(50),
---     city varchar(30),
---     last_update date
--- )
-
-alter table company2 add column create_date date;
+--changeset your.name:7
+--rollback DROP TABLE company;
+create table company3 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)

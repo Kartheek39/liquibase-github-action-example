6f6343ef5cdf502e7514262c59a77ba9334ac6bb
Kartheek39 <89856683+Kartheek39@users.noreply.github.com>
2023-12-29 13:29:41 +0530

    Update insert_data.sql

--- example/changelogs/insert_data.sql
+++ example/changelogs/insert_data.sql
@@ -1,3 +1,7 @@
 INSERT INTO public.person1
 (id, "name", address1, address2, city, country, month_date)
 select 1,'mahesh','abc','def','hyd','us',cast('2023-01-01' as date);
+
+INSERT INTO public.person1
+(id, "name", address1, address2, city, country, month_date)
+select 2,'kartheek','abc','alwal','hyd','India',cast('2023-12-29' as date);

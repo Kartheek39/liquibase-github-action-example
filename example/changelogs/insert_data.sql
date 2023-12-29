d50bb7e2d80afbf05968b54a025fa3017dc0058c
MushyamMahesh <80718387+MushyamMahesh@users.noreply.github.com>
2023-12-29 14:34:20 +0530

    Update insert_data.sql

--- example/changelogs/insert_data.sql
+++ example/changelogs/insert_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.person1
-(id, "name", address1, address2, city, country, month_date)
-select 1,'mahesh','abc','def','hyd','us',cast('2023-01-01' as date);
+(id, "name", address1, address2, city, country,month_date)
+select 3,'mahesh','abc','def','hyd','us',cast('2023-01-01' as date);
